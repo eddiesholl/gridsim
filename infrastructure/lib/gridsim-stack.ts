4568f4d263ce41ff22cb4ccf89c018d3775b0ddb
Eddie Sholl <eddie.s@agtuary.com>
2025-04-29 22:00:45 +1000

    API has domain, just need to fix CORS

--- infrastructure/lib/gridsim-stack.ts
+++ infrastructure/lib/gridsim-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
@@ -12,12 +15,22 @@ export class GridsimStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Get the hosted zone for eddit.io
+    const hostedZone = route53.HostedZone.fromLookup(this, "HostedZone", {
+      domainName: "eddit.io",
+    });
+
     // Create Lambda function for the API
     const apiHandler = new lambda.DockerImageFunction(this, "ApiHandler", {
       code: lambda.DockerImageCode.fromImageAsset(
         path.join(__dirname, "../../backend"),
         {
-          cmd: ["gridsim_backend.app.main.handler"],
+          cmd: [
+            "python3",
+            "-m",
+            "awslambdaric",
+            "gridsim_backend.app.main.handler",
+          ],
         }
       ),
       environment: {
@@ -37,15 +50,41 @@ export class GridsimStack extends cdk.Stack {
         allowMethods: apigateway.Cors.ALL_METHODS,
         allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
       },
+      domainName: {
+        domainName: "api.gridsim.eddit.io",
+        certificate: new acm.Certificate(this, "ApiCertificate", {
+          domainName: "api.gridsim.eddit.io",
+          validation: acm.CertificateValidation.fromDns(hostedZone),
+        }),
+      },
+    });
+
+    // Create DNS record for the API
+    new route53.ARecord(this, "ApiAliasRecord", {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.ApiGateway(api)
+      ),
+      recordName: "api.gridsim",
     });
 
     // Add Lambda integration to API Gateway
     const integration = new apigateway.LambdaIntegration(apiHandler);
-    api.root.addMethod("GET", integration);
-    api.root
-      .addResource("api")
-      .addResource("health")
-      .addMethod("GET", integration);
+
+    // Add routes for all FastAPI endpoints
+    const apiResource = api.root.addResource("api");
+
+    // Health check endpoint
+    apiResource.addResource("health").addMethod("GET", integration);
+
+    // Primitive endpoint
+    apiResource.addResource("primitive").addMethod("GET", integration);
+
+    // Add a proxy resource to handle all other routes
+    const proxyResource = apiResource.addProxy({
+      defaultIntegration: integration,
+      anyMethod: true,
+    });
 
     // Create S3 bucket for frontend
     const frontendBucket = new s3.Bucket(this, "FrontendBucket", {
