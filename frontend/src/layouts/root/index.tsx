90a5fe4b0f34753ef28417d72e5a7e90ba193a80
Eddie Sholl <eddie.s@agtuary.com>
2025-05-21 22:41:20 +1000

    Add styles for buttons

--- frontend/src/layouts/root/index.tsx
+++ frontend/src/layouts/root/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Title } from "@mantine/core";
+import { Box, Flex, Space, Title, UnstyledButton } from "@mantine/core";
 import { FileRouteTypes, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { MTLink } from "../../components/Link";
+import { GitHubCat } from "../../icons/GitHubCat";
 import styles from "./root.module.css";
 interface PathConfig {
   path: FileRouteTypes["to"];
@@ -34,12 +35,25 @@ export function RootComponent() {
       <Flex direction="column" h="100%">
         <Flex className={styles.header} p="sm">
           <Title order={1}>GridSim</Title>
-          <Flex gap="md">
+          <Flex gap="md" align="center">
             {paths.map(({ path, label }) => (
               <MTLink key={path} to={path} className={styles.link}>
                 {label}
               </MTLink>
             ))}
+            <UnstyledButton
+              component="a"
+              href="https://github.com/eddiesholl/gridsim"
+              target="_blank"
+              className={styles.control}
+            >
+              <GitHubCat
+                width={28}
+                height={"auto"}
+                className={styles.githubIcon}
+              />
+            </UnstyledButton>
+            <Space w="sm" />
           </Flex>
         </Flex>
         <Flex className={styles.main}>
