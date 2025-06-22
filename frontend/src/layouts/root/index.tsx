77b852e3b8285df1349178a1ae2dfc69a64666c7
Eddie Sholl <eddie.s@agtuary.com>
2025-06-22 22:00:38 +1000

    Mobile nav wip

--- frontend/src/layouts/root/index.tsx
+++ frontend/src/layouts/root/index.tsx
@@ -1,6 +1,15 @@
-import { Box, Flex, Space, Title, UnstyledButton } from "@mantine/core";
+import {
+  Box,
+  Burger,
+  Drawer,
+  Flex,
+  Title,
+  UnstyledButton,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { FileRouteTypes, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { useResponsiveMode } from "../../common/use-responsive-mode";
 import { MTLink } from "../../components/Link";
 import { GitHubCat } from "../../icons/GitHubCat";
 import styles from "./root.module.css";
@@ -29,18 +38,38 @@ const paths: PathConfig[] = [
 ];
 
 export function RootComponent() {
+  const isMobile = useResponsiveMode() === "mobile";
+  const [opened, { toggle }] = useDisclosure(false);
   return (
     <>
-      <TanStackRouterDevtools position="top-left" />
+      <TanStackRouterDevtools position="bottom-left" />
       <Flex direction="column" h="100%" className={styles.root}>
         <Flex className={styles.header} p="sm">
           <Title order={1}>GridSim</Title>
           <Flex gap="md" align="center">
-            {paths.map(({ path, label }) => (
-              <MTLink key={path} to={path} className={styles.link}>
-                {label}
-              </MTLink>
-            ))}
+            {isMobile && (
+              <>
+                <Burger
+                  opened={opened}
+                  onClick={toggle}
+                  hiddenFrom="sm"
+                  size="sm"
+                />
+                <Drawer opened={opened} onClose={toggle} title="Menu">
+                  {paths.map(({ path, label }) => (
+                    <MTLink key={path} to={path} className={styles.link}>
+                      {label}
+                    </MTLink>
+                  ))}
+                </Drawer>
+              </>
+            )}
+            {!isMobile &&
+              paths.map(({ path, label }) => (
+                <MTLink key={path} to={path} className={styles.link}>
+                  {label}
+                </MTLink>
+              ))}
             <UnstyledButton
               component="a"
               href="https://github.com/eddiesholl/gridsim"
@@ -53,7 +82,6 @@ export function RootComponent() {
                 className={styles.githubIcon}
               />
             </UnstyledButton>
-            <Space w="sm" />
           </Flex>
         </Flex>
         <Flex className={styles.main}>
